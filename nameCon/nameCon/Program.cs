b9455ad737be51aa128fd6cbdbb9e87c09533654
Bolaji-i <47947885+Bolaji-i@users.noreply.github.com>
2019-02-25 20:01:38 +0100

    Committed my first challenge solution.

---
+++ nameCon/nameCon/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace nameCon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name: Ilori Bolaji Daniels");
+            Console.WriteLine(" ");
+            Console.WriteLine($"Date: {DateTime.Now}");
+            Console.WriteLine(" ");
+            Console.ReadKey();
+        }
+    }
+}
